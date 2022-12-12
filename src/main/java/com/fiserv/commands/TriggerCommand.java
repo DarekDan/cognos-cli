4770f76eab0c5abb1565ddcd6c870904c71a0ad6
Danielewski <dariusz.danielewski@lm-c02yg0twjgh6.1dc.com>
2022-12-12 16:52:31 -0500

    Support for multiple triggers

--- src/main/java/com/fiserv/commands/TriggerCommand.java
+++ src/main/java/com/fiserv/commands/TriggerCommand.java
@@ -3,29 +3,34 @@ package com.fiserv.commands;
 import com.fiserv.CognosCliCommand;
 import com.fiserv.cognos.Session;
 import java.rmi.RemoteException;
+import java.util.List;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 import picocli.CommandLine.ParentCommand;
 
 @Command(name = "trigger", description = "Trigger an event in Cognos Analytics", mixinStandardHelpOptions = true)
 public class TriggerCommand implements Runnable {
 
-    @Option(names = {"-t", "--trigger"}, description = "TriggerName", order = 1)
-    private String triggerName;
+    @Parameters(description = "A space delimited list of names")
+    private List<String> triggerNames;
 
     @ParentCommand
     private CognosCliCommand parentCommand;
 
     @Override
     public void run() {
-        if(parentCommand.verbose){
-            System.out.printf("Invoking trigger %s%n", triggerName);
-        }
         Session session = new Session(parentCommand.dispatcherUrl);
-        if(session.login(parentCommand.userNamespace, parentCommand.userName, parentCommand.userPassword)){
+        if (session.login(parentCommand.userNamespace, parentCommand.userName, parentCommand.userPassword)) {
             try {
-                if(session.getEventService().trigger(triggerName)>0){
-                    System.out.printf("Trigger %s has been invoked%n",triggerName);
+                for (String triggerName : triggerNames) {
+                    if (parentCommand.verbose) {
+                        System.out.printf("Invoking trigger %s%n", triggerName);
+                    }
+                    if (session.getEventService()
+                        .trigger(triggerName) > 0) {
+                        System.out.printf("Trigger %s has been invoked%n", triggerName);
+                    }
                 }
             } catch (RemoteException e) {
                 throw new RuntimeException(e);
